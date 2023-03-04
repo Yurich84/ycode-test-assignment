62751acba8679cb28b9581d56bc173beb68db2d1
Yurich <Yurich84@gmail.com>
2023-03-04 17:18:18 +0000

    Add migration for products? orders, order_items tables Remove unnecessary migrations

---
+++ database/migrations/2023_03_04_013137_create_orders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('ycode_id');
+            $table->string('name');
+            $table->string('slug');
+            $table->string('customer_name');
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('address1');
+            $table->string('address2')->nullable();
+            $table->string('country');
+            $table->string('state');
+            $table->string('city');
+            $table->string('zip');
+            $table->decimal('shipping', 10);
+            $table->decimal('subtotal', 10);
+            $table->decimal('total', 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
