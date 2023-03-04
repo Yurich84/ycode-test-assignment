0a411869e86a39c8190d50bd48d256f6eb38f245
Yurich <Yurich84@gmail.com>
2023-03-04 13:00:19 +0000

    Add collections to config

--- config/ycode.php
+++ config/ycode.php
@@ -6,4 +6,12 @@ return [
 
     'token' => env('YCODE_API_TOKEN'),
 
+
+    'collections' => [
+        'products' => env('YCODE_PRODUCTS_COLLECTION', '63f52cf7b1c03'),
+        'orders' => env('YCODE_ORDERS_COLLECTION', '63f52de51fbf4'),
+        'order_items' => env('YCODE_ORDER_ITEMS_COLLECTION', '63f52df2283b1'),
+    ],
+
+
 ];
